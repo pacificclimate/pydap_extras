542019519b449822310b048178b4dd9dbf44666d
cairosanders <cairosanders@hotmail.com>
2021-04-01 11:22:12 -0700

    Add sql handler to app

--- pydap_extras/app.py
+++ pydap_extras/app.py
@@ -1,10 +1,19 @@
 import sys
 from paste.httpserver import serve
 from pydap.wsgi.ssf import ServerSideFunctions
-from pydap_extras.handlers import csv
+from pydap_extras.handlers import csv, sql
 
 if __name__ == "__main__":
-    csv._test()
-    application = csv.CSVHandler(sys.argv[1])
+    handler_type = sys.argv[1]
+
+    if handler_type == "csv":
+        csv._test()
+        application = csv.CSVHandler(sys.argv[2])
+    elif handler_type == "sql":
+        sql._test()
+        application = sql.CSVHandler(sys.argv[2])
+    else:
+        raise KeyError(f"No handler called {handler_type}")
+
     application = ServerSideFunctions(application)
     serve(application, port=8001)
