f9bc0932cda3300a88dd399d014936665f63405d
cairosanders <cairosanders@hotmail.com>
2021-04-12 09:52:58 -0700

    Add pcic handlers to app.py

--- pydap_extras/app.py
+++ pydap_extras/app.py
@@ -8,19 +8,28 @@ from pydap.wsgi.ssf import ServerSideFunctions
     "-h",
     "--handler-type",
     help="Convert between the handler-type format and the data model used by Pydap",
-    type=click.Choice(["sql", "csv"]),
+    type=click.Choice(["SQL", "CSV", "PcicSql", "RawPcicSql", "ClimoPcicSql"]),
 )
-@click.option("-f", "--filename", help="File containing data to pass to the handler")
+@click.option(
+    "-a",
+    "--args",
+    default=(None),
+    multiple=True,
+    help="Arguments to initiate the handler",
+)
 @click.option("-p", "--port", default=8001)
-def run_handler(handler_type, filename, port):
-    module = f"pydap_extras.handlers.{handler_type}"
-    _test = getattr(__import__(module, fromlist=["_test"]), "_test")
+def run_handler(handler_type, args, port):
+    if "pcic" in handler_type:
+        module = f"pydap_extras.handlers.pcic"
+    else:
+        module = f"pydap_extras.handlers.{handler_type.lower()}"
+        _test = getattr(__import__(module, fromlist=["_test"]), "_test")
+        _test()
 
-    handler_name = f"{handler_type.upper()}Handler"
+    handler_name = f"{handler_type}Handler"
     Handler = getattr(__import__(module, fromlist=[handler_name]), handler_name)
 
-    _test()
-    application = Handler(filename)
+    application = Handler(*args)
     application = ServerSideFunctions(application)
     serve(application, port=port)
 
