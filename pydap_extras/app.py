9301f38f30ccd306f750f8d51cc7645fc5592832
cairosanders <cairosanders@hotmail.com>
2021-04-13 10:40:15 -0700

    Add dictionary to map handler-type inpur to handler names

--- pydap_extras/app.py
+++ pydap_extras/app.py
@@ -8,7 +8,7 @@ from pydap.wsgi.ssf import ServerSideFunctions
     "-h",
     "--handler-type",
     help="Convert between the handler-type format and the data model used by Pydap",
-    type=click.Choice(["SQL", "CSV", "PcicSql", "RawPcicSql", "ClimoPcicSql"]),
+    type=click.Choice(["sql", "csv", "pcicsql", "rawpcicsql", "climopcicsql"]),
 )
 @click.option(
     "-a",
@@ -19,14 +19,22 @@ from pydap.wsgi.ssf import ServerSideFunctions
 )
 @click.option("-p", "--port", default=8001)
 def run_handler(handler_type, args, port):
+    handler_names = {
+        "sql": "SQL",
+        "csv": "CSV",
+        "pcicsql": "PcicSql",
+        "rawpcicsql": "RawPcicSql",
+        "climopcicsql": "ClimoPcicSql",
+    }
+
     if "pcic" in handler_type:
         module = f"pydap_extras.handlers.pcic"
     else:
-        module = f"pydap_extras.handlers.{handler_type.lower()}"
+        module = f"pydap_extras.handlers.{handler_type}"
         _test = getattr(__import__(module, fromlist=["_test"]), "_test")
         _test()
 
-    handler_name = f"{handler_type}Handler"
+    handler_name = f"{handler_names[handler_type]}Handler"
     Handler = getattr(__import__(module, fromlist=[handler_name]), handler_name)
 
     application = Handler(*args)
