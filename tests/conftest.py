bc8052f4cf71eaf4703e2c92d4e836cf4f65fd38
cairosanders <cairosanders@hotmail.com>
2021-04-23 13:36:22 -0700

    Copy handler and tests from pydap.htf5.handlers

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,13 @@ from datetime import datetime
 from collections import namedtuple
 from tempfile import NamedTemporaryFile
 
+from pkg_resources import resource_filename
+
+import pytest
+import numpy.random
+import h5py
+from pydap_extras.handlers.hdf5 import Hdf5Data
+
 import pytest
 import pycds
 from pycds import *
@@ -367,3 +374,39 @@ def raw_handler_get_vars_mock(monkeypatch, test_db_with_met_obs):
     monkeypatch.setattr(RawPcicSqlHandler, "get_vars", my_get_vars)
 
     return handler
+
+
+test_h5 = resource_filename('pydap.handlers.hdf5', 'data/test.h5')
+
+@pytest.fixture(scope="function", params=['/tasmax', '/tasmin', '/pr'])
+def hdf5data_instance_3d(request):
+    f = h5py.File(test_h5, 'r')
+    dst = f[request.param]
+    return Hdf5Data(dst)
+
+@pytest.fixture(scope="module", params=['/lat', '/lon', '/time'])
+def hdf5data_instance_1d(request):
+    f = h5py.File(test_h5, 'r')
+    dst = f[request.param]
+    return Hdf5Data(dst)
+
+# _All_ the variables should be iterable
+@pytest.fixture(scope="module", params=['/tasmax', '/tasmin', '/pr', '/lat', '/lon', '/time'])
+def hdf5data_iterable(request):
+    f = h5py.File(test_h5, 'r')
+    dst = f[request.param]
+    return Hdf5Data(dst)
+
+@pytest.fixture(scope="function")
+def hdf5_dst(request):
+    f = NamedTemporaryFile()
+    hf = h5py.File(f.name, driver='core', backing_store=False)
+    group = hf.create_group('foo')
+    dst = group.create_dataset('bar', (10, 10, 10), '=f8', maxshape=(None, 10, 10))
+    dst[:,:,:] = numpy.random.rand(10, 10, 10)
+
+    def fin():
+        hf.close()
+        os.remove(f.name)
+    request.addfinalizer(fin)
+    
---
+++ tests/test_hdf4_handlers.py
@@ -0,0 +1,92 @@
+from pkg_resources import resource_filename
+
+import h5py
+import numpy
+from webob.request import Request
+
+from pydap.handlers.hdf5 import Hdf5Data
+
+def test_can_instantiate(hdf5_dst):
+    var = Hdf5Data(hdf5_dst)
+    assert var.shape == (10, 10, 10)
+
+def test_can_iterate_on_unsliced(hdf5data_iterable):
+    for data in hdf5data_iterable:
+        pass
+    assert True
+
+def test_can_iterate_on_sliced_major(hdf5data_instance_3d):
+    i = 0
+    for data in hdf5data_instance_3d[5:10,:,:]:
+        i += 1
+    assert i == 5
+
+def test_can_iterate_on_sliced_minor(hdf5data_instance_3d):
+    i = 0
+    for data in hdf5data_instance_3d[:,1:2,3:4]:
+        i += 1
+    assert i == 10
+
+def test_can_iterate_on_sliced_major_minor(hdf5data_instance_3d):
+    i = 0
+    for data in hdf5data_instance_3d[0:2,3:4,5:6]:
+        i += 1
+    assert i == 2
+        
+def test_shape_of_unsliced_3d(hdf5data_instance_3d):
+    x = hdf5data_instance_3d
+    assert x.shape == (10, 10, 10)
+
+def test_shape_of_sliced_3d(hdf5data_instance_3d):
+    x = hdf5data_instance_3d
+    assert x[5:10,:,:].shape == (5, 10, 10)
+
+def test_shape_of_unsliced_1d(hdf5data_instance_1d):
+    x = hdf5data_instance_1d
+    assert x.shape == (10,)
+
+def test_shape_of_sliced_1d(hdf5data_instance_1d):
+    x = hdf5data_instance_1d
+    assert x[5:10].shape == (5,)
+
+# Unless 1d variables are of unlimited dimensions, you should get all of their
+# output on the first iteration in a numpy array
+def test_1d_iteration(hdf5data_instance_1d):
+    x = hdf5data_instance_1d
+    for i in iter(x):
+        if hdf5data_instance_1d.var.maxshape == (None,):
+            assert type(i) == numpy.float64
+        else:
+            assert type(i) == numpy.ndarray
+            assert len(i) == len(hdf5data_instance_1d.var)
+
+def test_can_slice_a_sliced_dataset(hdf5data_instance_3d):
+    x = hdf5data_instance_3d
+    subset = x[5:10,:,:][1:2,:,:]
+    assert subset.shape == (1, 10, 10)
+    
+def test_the_bounds():
+    test_bounds = resource_filename('pydap.handlers.hdf5', 'data/bounds.h5')
+    from pydap.handlers.hdf5 import HDF5Handler
+    app = HDF5Handler(test_bounds)
+    req = Request.blank('/bounds.nc.ascii?climatology_bounds')
+    resp = req.get_response(app)
+    assert resp.status == '200 OK'
+    assert resp.body == '''climatology_bounds.climatology_bounds
+[0.0, 10988.0]
+[31.0, 11017.0]
+[59.0, 11048.0]
+[90.0, 11078.0]
+[120.0, 11109.0]
+[151.0, 11139.0]
+[181.0, 11170.0]
+[212.0, 11201.0]
+[243.0, 11231.0]
+[273.0, 11262.0]
+[304.0, 11292.0]
+[334.0, 11323.0]
+[0.0, 11323.0]
+climatology_bounds.time
+climatology_bounds.bnds
+[0.0, 0.0]
+'''
